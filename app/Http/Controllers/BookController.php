a2742b9700e6dcda51156eab92e18738d720aa84
chantapat.so <chantapat.so@t2pco.com>
2020-05-28 12:59:54 +0700

    Add insert and update book

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -9,9 +9,19 @@ class BookController extends Controller
 {
     public function store(Request $request)
     {
-        Book::create([
-            'title' => $request->get('title'),
-            'author' => $request->get('author')
+        Book::create($this->getValidate($request));
+    }
+
+    public function update(Request $request, Book $book)
+    {
+        $book->update($this->getValidate($request));
+    }
+
+    protected function getValidate(Request $request): array
+    {
+        return $request->validate([
+            'title'  => 'required',
+            'author' => 'required',
         ]);
     }
 }
